a1a9a58e41ab55f39045e26663d78d5d6fef981b
CodeGamel <collinsgamel@gmail.com>
2024-07-22 15:26:55 -0700

    final commit

---
+++ loop_condition.py
@@ -0,0 +1,17 @@
+#-----------------------------------------------------
+Flag = True
+
+while Flag :
+    user = input('Whats your favorite movie?')
+    if user == 'Spiderman 2' :
+        break
+
+#-----------------------------------------------------
+
+iteration = 1
+
+max_interations = 5
+
+while iteration <= max_interations:
+    print(f'Iteration{iteration}')
+    iteration += 1
